1d24f6498722bc93c795303e2dfdce407ebbb13d
Ex-Kyuto <86765295+kawaii-ghost@users.noreply.github.com>
2022-12-21 17:16:00 +0700

    Add files via upload

---
+++ nt.asm
@@ -0,0 +1,36 @@
+extern NtWriteFile
+extern NtTerminateProcess
+
+%define NtCurrentPeb() gs:[0x60]
+%define ProcessParameter 32
+%define StandardOutput 40
+%define NtCurrentProcess() -1
+
+section .rdata
+	msg db `Hello, world!\n\0`
+        
+section .bss
+	IoStatusBlock resb 16
+        
+section .text
+	global WinMain
+WinMain:
+        sub rsp, 88
+        
+	mov rcx, NtCurrentPeb()
+	mov rcx, ProcessParameter[rcx]
+	mov rcx, StandardOutput[rcx]
+	xor edx, edx
+	xor r8d, r8d
+	xor r9d, r9d
+	mov qword 32[rsp], IoStatusBlock
+	mov qword 40[rsp], msg
+	mov qword 48[rsp], 14
+	mov qword 56[rsp], 0
+	mov qword 64[rsp], 0
+	call NtWriteFile
+
+	mov rcx, NtCurrentProcess()
+	xor edx, edx
+	call NtTerminateProcess
+	int3
