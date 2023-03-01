ad8dd7471a02a14a02d8b747ae9b62c1571d5898
kawaii-ghost <6h8fn1zo@duck.com>
2023-03-01 09:42:09 +0700

    Update nt.asm

--- nt.asm
+++ nt.asm
@@ -9,27 +9,26 @@ extern RtlExitUserProcess
 
 section .rdata
 	msg db `Hello, world!\n\0`
-section .bss
-	IoStatusBlock resb 16
 section .text
 	global main
 main:
-        sub rsp, 72
+        sub rsp, 86
 	mov rcx, NtCurrentPeb()
 	mov rcx, ProcessParameter[rcx]
-	mov rcx, StandardOutput[rcx]
-	xor edx, edx
-	xor r8d, r8d
-	xor r9d, r9d
-	mov qword 32[rsp], IoStatusBlock
+	mov rcx, StandardOutput[rcx] ; hStdOut
+	xor edx, edx ; NULL
+	xor r8d, r8d ; NULL
+	xor r9d, r9d ; NULL
+	lea r10, 72[rsp]
+	mov qword 32[rsp], r10 ; &IoStatusBlock
 	mov qword 40[rsp], msg
 	mov qword 48[rsp], 14
-	mov qword 56[rsp], 0
-	mov qword 64[rsp], 0
+	mov qword 56[rsp], 0 ; NULL
+	mov qword 64[rsp], 0 ; NULL
 	call NtWriteFile
 
-	; There are two ways to TerminateProcess either using NtTerminateProcess directly
-	; or via RtlExitUserProcess but the second one is much safer
+	; There are two ways to terminate the calling process either using NtTerminateProcess directly
+	; or via RtlExitUserProcess but the second one is doing cleanup before terminate.
 	; mov rcx, NtCurrentProcess()
 	; xor edx, edx
 	; call NtTerminateProcess
